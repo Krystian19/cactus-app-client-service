212931a839c89dabcb568b826c1ba236e368475d
Jan Guzman <janguzman19@gmail.com>
2018-05-07 07:54:07 -0400

    Adding constructor method to App class

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    constructor() {
+        super()
+    }
+
     render() {
         return <h1>App component</h1>;
     }
