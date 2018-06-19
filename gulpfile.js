dd6db48e3f869d344481cff63d4d6310891c0077
Jan Guzman <janguzman19@gmail.com>
2018-06-19 07:36:49 -0400

    Updating gulpfile to include node_module dependencies in the sass build

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 'use strict';
 
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const minifyCSS = require('gulp-clean-css')
-const ugligy = require('gulp-uglify')
-const rename = require('gulp-rename')
-const changed = require('gulp-changed')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
-const origin_watch = './src/scss/**/*.scss'
-const origin_target = './src/scss/main.scss'
-const destination = './public/css'
+const originWatch = './src/scss/**/*.scss';
+const originTarget = './src/scss/main.scss';
+const destination = './public/css';
 
 // Compile scss
 gulp.task('build', () => {
-    gulp.src(origin_target)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(destination))
-})
+	gulp.src(originTarget)
+		.pipe(sass({
+			includePaths: ['node_modules'],
+		}).on('error', sass.logError))
+		.pipe(minifyCSS())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(destination));
+});
 
 // Detect changes in scss
 gulp.task('watch', () => {
-    gulp.start('build')
-    gulp.watch(origin_watch, ['build'])
-})
+	gulp.start('build');
+	gulp.watch(originWatch, ['build']);
+});
 
 // Run tasks
-gulp.task('default', ['watch'])
+gulp.task('default', ['watch']);
