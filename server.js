aa57ce2cf509ac1e7db685a45704fb7552f496db
Jan Guzman <janfrancisco19@gmail.com>
2018-10-28 22:56:30 -0400

    Adding proxy for graphql requests

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const express = require('express');
+const requestProxy = require("express-request-proxy");
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, 'node_modules')));
+
+app.post(
+  "/graphql",
+  requestProxy({
+    url: "http://backend:3000/",
+    query: {},
+    headers: {},
+  })
+);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
