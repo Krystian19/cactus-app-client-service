1e2c548f3d4f21ca554dfefc7b0d9b3e9eb0d010
Jan Guzman <janfrancisco19@gmail.com>
2018-10-29 13:48:37 -0400

    Working with the dashboard markup and styling

--- src/components/views/Dashboard/index.js
+++ src/components/views/Dashboard/index.js
@@ -10,8 +10,254 @@ export default class DashboardView extends Component {
     return (
       <div className="main-container">
         <Sidebar />
-        <div className="main-content no-padding no-margin">
-          <h1>This is the search view</h1>
+        <div className="main-content no-padding">
+          {/* Block start */}
+          <div className="video-block">
+            <div className="video-block-title">
+              <div className="video-block-title-head">
+                <span role="img" aria-label="">🔥 right now</span>
+              </div>
+              <div className="video-block-title-options">
+                <a href="http://www.google.com" className="button">View all</a>
+              </div>
+            </div>
+            <div className="video-block-content">
+              <div className="anime-small-thumbnail-list">
+
+                <div className="anime-small-thumbnail big">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/28163fr0Ltd5.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Boku No hero Academia 3rd season </div>
+                      <div className="detail-container"> Ep. 16 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail small">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/64i2RVIJeV.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> One Piece </div>
+                      <div className="detail-container"> Ep. 266 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail small">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/2909rbi6tQo0.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Overlord III </div>
+                      <div className="detail-container"> Ep. 11 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail small">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/1176b7Sk5J93.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Haikyuu!! Second Season </div>
+                      <div className="detail-container"> Ep. 8 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail small">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/226eH0orQ53.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Fullmetal Alchemist: Brotherhood </div>
+                      <div className="detail-container"> Ep. 32 </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          {/* Block end */}
+
+          {/* Block start */}
+          <div className="video-block">
+            <div className="video-block-title">
+              <div className="video-block-title-head">
+                <span>New Episodes</span>
+              </div>
+              <div className="video-block-title-options">
+                <a href="http://www.google.com" className="button">View all</a>
+              </div>
+            </div>
+            <div className="video-block-content">
+              <div className="anime-small-thumbnail-list">
+
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/2808dEyIDBR6.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> No Game No Life: Zero lorem </div>
+                      <div className="detail-container"> Ep. 1 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/28193ESeF49G.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Shokugeki no Souma 3rd season </div>
+                      <div className="detail-container"> Ep. 12 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/28174OVMKYWJ.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Tokyo Ghoul 3rd season </div>
+                      <div className="detail-container"> Ep. 11 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/2908UZUvpOMN.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Shingeki no Kyojin Season 3 </div>
+                      <div className="detail-container"> Ep. 10 </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/1105x2klHygG.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Fate/stay night 2nd Season </div>
+                      <div className="detail-container"> Ep. 13 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/282mpLwPxe8.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Black Lagoon: 2nd season </div>
+                      <div className="detail-container"> Ep. 10 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/2823lknkr4jp.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Wotaku ni Koi wa Muzukashii </div>
+                      <div className="detail-container"> Ep. 4 </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="anime-small-thumbnail">
+                  <div className="cover">
+                    <img src="https://cdn.masterani.me/wallpaper/0/445H9PlfsZS.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+                    <div className="overlay hover_hidden darken">
+                      <svg className="play" viewBox="0 0 24 24">
+                        <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="info">
+                    <div className="title">
+                      <div className="title-container"> Yahari Ore no Seishun lorem </div>
+                      <div className="detail-container"> Ep. 5 </div>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+          {/* Block end */}
         </div>
       </div>
     );
