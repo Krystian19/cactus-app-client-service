039a3890cc44bf180c70c75c078a986468e94af1
Jan Guzman <janfrancisco19@gmail.com>
2018-10-29 08:08:17 -0400

    Including eslint into the project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/prefer-stateless-function": "off",
+    "import/prefer-default-export": "off",
+    "strict": 0,
+    "react/prop-types": 0
+  },
+  "globals": {
+    "window": true,
+    "document": true,
+    "console": true,
+    "localStorage": true,
+  }
+};
