5024c88b7616c893b9e0242e006c5683fa8cde29
Jan Guzman <janfrancisco19@gmail.com>
2018-11-01 15:45:30 -0400

    Dividing view parts into components

---
+++ src/components/shared_components/HottestVideoBlock/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+// ({ Episodes }) => (
+const HottestVideoBlock = () => (
+  <div className="video-block">
+    <div className="video-block-title">
+      <div className="video-block-title-head">
+        <span role="img" aria-label="">🔥 right now</span>
+      </div>
+      <div className="video-block-title-options">
+        <a href="http://www.google.com" className="button">View all</a>
+      </div>
+    </div>
+    <div className="video-block-content">
+      <div className="anime-small-thumbnail-list">
+
+        <div className="anime-small-thumbnail big">
+          <div className="cover">
+            <img src="https://cdn.masterani.me/wallpaper/0/28163fr0Ltd5.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+            <div className="overlay hover_hidden darken">
+              <svg className="play" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="info">
+            <div className="title">
+              <div className="title-container"> Boku No hero Academia 3rd season </div>
+              <div className="detail-container"> Ep. 16 </div>
+            </div>
+          </div>
+        </div>
+        <div className="anime-small-thumbnail small">
+          <div className="cover">
+            <img src="https://cdn.masterani.me/wallpaper/0/64i2RVIJeV.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+            <div className="overlay hover_hidden darken">
+              <svg className="play" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="info">
+            <div className="title">
+              <div className="title-container"> One Piece </div>
+              <div className="detail-container"> Ep. 266 </div>
+            </div>
+          </div>
+        </div>
+        <div className="anime-small-thumbnail small">
+          <div className="cover">
+            <img src="https://cdn.masterani.me/wallpaper/0/2909rbi6tQo0.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+            <div className="overlay hover_hidden darken">
+              <svg className="play" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="info">
+            <div className="title">
+              <div className="title-container"> Overlord III </div>
+              <div className="detail-container"> Ep. 11 </div>
+            </div>
+          </div>
+        </div>
+        <div className="anime-small-thumbnail small">
+          <div className="cover">
+            <img src="https://cdn.masterani.me/wallpaper/0/1176b7Sk5J93.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+            <div className="overlay hover_hidden darken">
+              <svg className="play" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="info">
+            <div className="title">
+              <div className="title-container"> Haikyuu!! Second Season </div>
+              <div className="detail-container"> Ep. 8 </div>
+            </div>
+          </div>
+        </div>
+        <div className="anime-small-thumbnail small">
+          <div className="cover">
+            <img src="https://cdn.masterani.me/wallpaper/0/226eH0orQ53.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+            <div className="overlay hover_hidden darken">
+              <svg className="play" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="info">
+            <div className="title">
+              <div className="title-container"> Fullmetal Alchemist: Brotherhood </div>
+              <div className="detail-container"> Ep. 32 </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+);
+
+export default HottestVideoBlock;
