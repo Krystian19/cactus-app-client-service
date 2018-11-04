39204bbe385def1bfb65d79c326694a6a91dc786
Jan Guzman <janfrancisco19@gmail.com>
2018-11-04 12:05:43 -0400

    Setting the hydrate option insted of render the ReactDom option

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'babel-polyfill'; // Async-Await faeatures
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 
-ReactDOM.render(
+hydrate(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
