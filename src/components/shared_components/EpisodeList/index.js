d0985cc01585419793fdd8806e1a7e36bb70f5cf
Jan Guzman <janfrancisco19@gmail.com>
2018-11-26 20:39:23 -0400

    Working with the hottest episodes big view

---
+++ src/components/shared_components/EpisodeList/index.js
@@ -0,0 +1,185 @@
+import React from 'react';
+
+// ({ Episodes }) => (
+const EpisodeList = (props) => {
+  const { props: { episodes, history, title } } = props;
+
+  return (
+    <div className="video-block">
+      <div className="video-block-title">
+        <div className="video-block-title-head">
+          <span>{title}</span>
+        </div>
+      </div>
+      <div className="video-block-content">
+        <div className="anime-small-thumbnail-list">
+          {episodes.map((episode, index) => (
+            <div
+              key={episode.id}
+              className="anime-small-thumbnail"
+              onClick={() => history.push(`/anime/video/${episode.id}`)}
+              onKeyPress={() => history.push(`/anime/video/${episode.id}`)}
+              role="menuitem"
+              tabIndex={index}
+            >
+              <div className="cover">
+                <img
+                  src={`/img_cdn/${episode.Season.background}`}
+                  alt="thumbnail"
+                  className="anime-small-thumbnail"
+                />
+                <div className="overlay hover_hidden darken">
+                  <svg className="play" viewBox="0 0 24 24">
+                    <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="info">
+                <div className="title">
+                  <div className="title-container">
+                    {`${episode.Season.title} (Season ${episode.Season.seasonOrder})`}
+                  </div>
+                  <div className="detail-container">
+                    {`Ep. ${episode.episodeOrder}`}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EpisodeList;
+
+// <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/2808dEyIDBR6.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> No Game No Life: Zero lorem </div>
+//               <div className="detail-container"> Ep. 1 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/28193ESeF49G.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Shokugeki no Souma 3rd season </div>
+//               <div className="detail-container"> Ep. 12 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/28174OVMKYWJ.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Tokyo Ghoul 3rd season </div>
+//               <div className="detail-container"> Ep. 11 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/2908UZUvpOMN.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Shingeki no Kyojin Season 3 </div>
+//               <div className="detail-container"> Ep. 10 </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/1105x2klHygG.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Fate/stay night 2nd Season </div>
+//               <div className="detail-container"> Ep. 13 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/282mpLwPxe8.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Black Lagoon: 2nd season </div>
+//               <div className="detail-container"> Ep. 10 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/2823lknkr4jp.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Wotaku ni Koi wa Muzukashii </div>
+//               <div className="detail-container"> Ep. 4 </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="anime-small-thumbnail">
+//           <div className="cover">
+//             <img src="https://cdn.masterani.me/wallpaper/0/445H9PlfsZS.jpg" alt="thumbnail" className="anime-small-thumbnail" />
+//             <div className="overlay hover_hidden darken">
+//               <svg className="play" viewBox="0 0 24 24">
+//                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+//               </svg>
+//             </div>
+//           </div>
+//           <div className="info">
+//             <div className="title">
+//               <div className="title-container"> Yahari Ore no Seishun lorem </div>
+//               <div className="detail-container"> Ep. 5 </div>
+//             </div>
+//           </div>
+//         </div>
--- src/components/views/HottestEpisodes/index.js
+++ src/components/views/HottestEpisodes/index.js
@@ -1,5 +1,29 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import React, { Component } from 'react';
 import Sidebar from '../../shared_components/Sidebar';
+import EpisodeList from '../../shared_components/EpisodeList';
+import LoadingSpinner from '../../shared_components/LoadingSpinner';
+
+const HottestEpisodesQuery = gql`
+  query {
+    getHottestEpisodes {
+      id,
+      thumbnail,
+      episodeOrder,
+      Season {
+        id,
+        seasonOrder,
+        title,
+        background,
+        Anime {
+          id,
+          title
+        }
+      }
+    }
+  }
+`;
 
 export default class HottestEpisodes extends Component {
   // constructor() {
@@ -11,9 +35,34 @@ export default class HottestEpisodes extends Component {
     return (
       <div className="main-container">
         <Sidebar props={{ history }} />
-        <div className="main-content">
-          <h1>These are the hottest episodes</h1>
-        </div>
+
+        <Query query={HottestEpisodesQuery}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return (
+                <div className="main-content no-padding">
+                  <LoadingSpinner />
+                </div>
+              );
+            }
+
+            if (error) return <p>Error :(</p>;
+
+            console.log(data);
+            return (
+              <div className="main-content no-padding">
+                <EpisodeList
+                  props={{
+                    title: 'Hottest episodes',
+                    episodes: data.getHottestEpisodes,
+                    history,
+                  }}
+                />
+              </div>
+            );
+          }}
+        </Query>
+
       </div>
     );
   }
