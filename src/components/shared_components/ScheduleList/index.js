67db659361fefa0da1e7ae7aced5fdd37c080e13
Jan Guzman <janfrancisco19@gmail.com>
2018-12-12 16:33:29 -0400

    Working with the getDayOfTheWeek util method

--- src/components/shared_components/ScheduleList/index.js
+++ src/components/shared_components/ScheduleList/index.js
@@ -3,13 +3,7 @@ import moment from 'moment';
 
 import ClientRender from '../ClientRenderer';
 import JSTToLocalTime from '../../../utils/JSTtoLocalTime';
-
-/**
-* @author Jan Guzman <janfrancisco19@gmail.com>
-* @desc Returns the current day of the week as an integer
-* @returns Integer Day of the week. E.g: 1 for monday, 7 for sunday, etc ...
-*/
-const getDayOfTheWeek = () => moment().isoWeekday();
+import getDayOfTheWeek from '../../../utils/getDayOfTheWeek';
 
 /**
 * @author Jan Guzman <janfrancisco19@gmail.com>
