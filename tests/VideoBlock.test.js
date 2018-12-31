0a2e23b4e4238a44d3dc272a4c090634932b1055
Jan Guzman <janfrancisco19@gmail.com>
2018-12-31 14:37:23 -0400

    Working with the tests structure

--- tests/VideoBlock.test.js
+++ tests/VideoBlock.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { test, expect } from 'jest';
+
+const { test, expect } = window;
 
 test('Tests are working well', () => {
   expect(true).toBe(true);
