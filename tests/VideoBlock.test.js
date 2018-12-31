558a4c13c35583ba5cec73aa3a871b5572b63793
Jan Guzman <janfrancisco19@gmail.com>
2018-12-31 14:33:00 -0400

    Including the basic boilerplate for tests

---
+++ tests/VideoBlock.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+// import { test, expect } from 'jest';
+
+test('Tests are working well', () => {
+  expect(true).toBe(true);
+});
