c782300affc574c6ed9e0289bde26ee25adb8358
Jan Guzman <janfrancisco19@gmail.com>
2019-02-11 18:20:12 -0400

    Including proxy routes for the web server

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,6 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from 'path';
+import requestProxy from 'express-request-proxy';
+
+// Root path of the project
+const rootPath = path.resolve(__dirname, '..', '..');
+
+// External service URLs
+const backendServiceUrl = process.env.BACKEND_SERVICE_URL || 'http://backend:3000/';
+const imageServiceUrl = process.env.IMG_CDN_SERVICE_URL || 'http://img_cdn:3000/';
+const videoServiceUrl = process.env.VIDEO_CDN_SERVICE_URL || 'http://video_cdn:3000/';
 
 class Server {
   public app: express.Application;
@@ -12,36 +21,86 @@ class Server {
     this.config();
   }
 
-  private proxies(): void {
-    // This method sets the proxies for the 3rd party services
-  }
-
-  private static(): void {
-    // Setup static files for public usage
-    this.app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
-    this.app.use(express.static(path.resolve(__dirname, '..', '..', 'node_modules')));
-  }
-
+  /**
+   * @description Configuration of the express web server
+   */
   private config(): void {
     // support application/json type post data
     this.app.use(bodyParser.json());
+
     //support application/x-www-form-urlencoded post data
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
-    // Setup static files for public use
+    // Setup static directories for public use
     this.static();
 
-    // Setup proxies with 3rd party services
-    this.proxies();
-
-    // Setup routes
+    // Setup web routes
     this.routes();
   }
 
+  /**
+   * @description Setup static directories for public usage
+   */
+  private static(): void {
+    this.app.use(express.static(path.resolve(rootPath, 'public')));
+    this.app.use(express.static(path.resolve(rootPath, 'node_modules')));
+  }
+
+  /**
+   * @description Setup web routes for the express server
+   */
   private routes(): void {
+    // Setup proxies for 3rd party services
+    this.proxies();
+
+    // Setup standart routes
     this.app.get('**', (req, res) => {
-      res.sendFile(path.join(__dirname, '..', '..' ,'public', 'index.html'));
+      res.sendFile(path.join(rootPath, 'public', 'index.html'));
     });
+  }
+
+  /**
+   * @description Setup proxy routes for external services
+   */
+  private proxies(): void {
+    this.app.post(
+      '/graphql',
+      requestProxy({
+        url: backendServiceUrl,
+        query: {},
+        headers: {},
+      }),
+    );
+
+    this.app.get(
+      '/img_cdn/:img_name',
+      (req, res, next) => {
+        const imgName = req.params.img_name;
+
+        const proxy = requestProxy({
+          url: imageServiceUrl + imgName,
+          query: {},
+          headers: {},
+        });
+
+        proxy(req, res, next);
+      },
+    );
+
+    this.app.get(
+      '/video_cdn/:video_name',
+      (req, res, next) => {
+        const videoName = req.params.video_name;
+
+        const proxy = requestProxy({
+          url: videoServiceUrl + videoName,
+          query: req.query,
+          headers: req.headers,
+        });
+
+        proxy(req, res, next);
+      },
+    );
   }
 
 }
