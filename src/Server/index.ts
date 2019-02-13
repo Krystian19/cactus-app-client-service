b5ed78c6ccf3ce7aa076cda75598d09d830b9099
Jan Guzman <janfrancisco19@gmail.com>
2019-02-13 15:54:54 -0400

    Fixing casing problem

--- src/Server/index.ts
+++
@@ -1,8 +0,0 @@
-import Server from "./server";
-
-// Web server's working port env variable
-const PORT: Number = Number(process.env.PORT || 3000);
-
-Server.listen(PORT, () => {
-  console.log(`Web client server listening @ http://localhost:${PORT}`);
-});
