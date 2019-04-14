694d200702db6e697b8cdd67068d9cb07ec9c976
Jan Guzman <janfrancisco19@gmail.com>
2019-04-13 23:50:02 -0400

    The categories view now has a proper pagination mechanism

--- src/app/components/shared/CategoriesBlock/index.tsx
+++ src/app/components/shared/CategoriesBlock/index.tsx
@@ -5,9 +5,7 @@ import {
   withRouter,
 } from 'react-router-dom';
 
-import LazyImage from '../LazyImage';
-import PaginationBox from '../../shared/PaginationBox';
-
+import LazyImage from '../LazyImage'
 import Genre from '../../@types/Genre';
 
 type PropType = RouteComponentProps<{}> & {
@@ -16,91 +14,52 @@ type PropType = RouteComponentProps<{}> & {
   viewAllLink?: String,
 }
 
-type StateType = {
-  currentPage: Number,
-}
+const CategoriesBlock = (props: PropType) => {
+  const {
+    categories,
+    title,
+    viewAllLink
+  } = props;
 
-// How many records should be shown per page.
-const pageCount = 8;
-
-class CategoriesBlock extends React.Component<PropType, StateType> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      // Pagination state values
-      currentPage: 0,
-    }
-  }
-
-  PageForward = () => {
-    const { currentPage } = this.state;
-    this.setState({ currentPage: (Number(currentPage) + 1) });
-  }
-
-  PageBackwards = () => {
-    const { currentPage } = this.state;
-
-    // Can't go backwards if page is already 0
-    if (currentPage === 0) return;
-
-    this.setState({ currentPage: (Number(currentPage) - 1) });
-  }
-
-  setCurrentPage = (page) => {
-    // If arg is null then ignore it
-    if ((page === null) || (page === undefined)) return;
-
-    this.setState({ currentPage: page });
-  }
-
-  render() {
-    const {
-      categories,
-      title,
-      viewAllLink
-    } = this.props;
-
-    return (
-      <div className="video-block">
-        <div className="video-block-title">
-          <div className="video-block-title-head">
-            <span>{title}</span>
-          </div>
-          {viewAllLink && (
-            <Link to={String(viewAllLink)} className="button">
-              View all
-            </Link>
-          )}
+  return (
+    <div className="video-block">
+      <div className="video-block-title">
+        <div className="video-block-title-head">
+          <span>{title}</span>
         </div>
-        <div className="video-block-content">
-          <div className="anime-small-thumbnail-list">
+        {viewAllLink && (
+          <Link to={String(viewAllLink)} className="button">
+            View all
+          </Link>
+        )}
+      </div>
+      <div className="video-block-content">
+        <div className="anime-small-thumbnail-list">
 
-            {categories.map(category => (
-              <div
-                key={Number(category.id)}
-                className="anime-small-thumbnail small category"
-              >
-                <div className="cover">
-                  <LazyImage
-                    src={`/img_cdn/${category.thumbnail}`}
-                    errorSrc="/img/category_placeholder.png"
-                    alt="thumbnail"
-                    className="anime-small-thumbnail fade-in"
-                    noLoadingSpinner
-                  />
-                  <div className="overlay">
-                    <span className="category_title">{category.title}</span>
-                  </div>
+          {categories.map(category => (
+            <div
+              key={Number(category.id)}
+              className="anime-small-thumbnail small category"
+            >
+              <div className="cover">
+                <LazyImage
+                  src={`/img_cdn/${category.thumbnail}`}
+                  errorSrc="/img/category_placeholder.png"
+                  alt="thumbnail"
+                  className="anime-small-thumbnail fade-in"
+                  noLoadingSpinner
+                />
+                <div className="overlay">
+                  <span className="category_title">{category.title}</span>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
 
-          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withRouter(CategoriesBlock);
--- src/app/components/views/Categories/index.tsx
+++ src/app/components/views/Categories/index.tsx
@@ -1,26 +1,73 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import React from 'react';
+
 import CategoriesBlock from '../../shared/CategoriesBlock';
 import LoadingSpinner from '../../shared/LoadingSpinner';
+import PaginationBox from '../../shared/PaginationBox';
 
 const GenresEpisodesQuery = gql`
-  query {
-    getGenres {
+  query($pageCount:Int, $currentPage:Int) {
+    getGenres(limit:$pageCount, offset: $currentPage) {
       rows {
         id,
         title,
         thumbnail
       },
       count
-    }
+    },
   }
 `;
 
-export default class Categories extends React.Component {
+type StateType = {
+  currentPage: Number,
+}
+
+// How many records should be shown per page.
+const pageCount = 8;
+
+export default class Categories extends React.Component<{}, StateType> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Pagination state values
+      currentPage: 0,
+    }
+  }
+
+  PageForward = () => {
+    const { currentPage } = this.state;
+    this.setState({ currentPage: (Number(currentPage) + 1) });
+  }
+
+  PageBackwards = () => {
+    const { currentPage } = this.state;
+
+    // Can't go backwards if page is already 0
+    if (currentPage === 0) return;
+
+    this.setState({ currentPage: (Number(currentPage) - 1) });
+  }
+
+  setCurrentPage = (page) => {
+    // If arg is null then ignore it
+    if ((page === null) || (page === undefined)) return;
+
+    this.setState({ currentPage: page });
+  }
+
   render() {
+    const { currentPage } = this.state;
+
     return (
-      <Query query={GenresEpisodesQuery}>
+      <Query
+        query={GenresEpisodesQuery}
+        variables={{
+          pageCount: pageCount,
+          currentPage: (Number(currentPage) * pageCount),
+        }}
+      >
         {({ loading, error, data }) => {
           if (loading) {
             return (
@@ -39,6 +86,28 @@ export default class Categories extends React.Component {
                 title={'Categories'}
                 categories={data.getGenres.rows}
               />
+              {
+                data.getGenres.rows.length !== 0
+                && (
+                  <PaginationBox
+                    pageCount={pageCount}
+                    itemCount={data.getGenres.count}
+                    currentPage={currentPage}
+                    goForwardCB={() => {
+                      const lastPage = Math.ceil(
+                        data.getGenres.count / pageCount,
+                      );
+
+                      // If this is the last page, don't go forward
+                      if ((Number(currentPage) + 1) === lastPage) return;
+
+                      this.PageForward();
+                    }}
+                    goBackwardsCB={() => this.PageBackwards()}
+                    setCurrentPageCB={this.setCurrentPage}
+                  />
+                )
+              }
             </div>
           );
         }}
