ddfacf4974b637f2f917f0b752302efdc8d5ff49
Jan Guzman <janfrancisco19@gmail.com>
2019-12-20 16:09:52 +0100

    Trigger change to work with travis

--- wait.sh
+++ wait.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Waits until cactus_bff container is healthy. (Is only used in Travis.CI).
+# Waits until cactus_bff container is healthy. (Is only used in Travis.CI)
 until [ "`docker inspect -f {{.State.Health.Status}} cactus_bff`" == "healthy" ]; do
   sleep 10;
   echo "Wait until cactus bff is ready";
