0509aa259c6795e12a0a15b5c99218f3ab3b0431
Jan Guzman <janfrancisco19@gmail.com>
2019-12-20 16:05:29 +0100

    Working with travis

--- wait.sh
+++ wait.sh
@@ -2,6 +2,6 @@
 
 # Waits until cactus_bff container is healthy. (Is only used in Travis.CI).
 until [ "`docker inspect -f {{.State.Health.Status}} cactus_bff`" == "healthy" ]; do
-  sleep 0.1;
+  sleep 10;
   echo "Wait until cactus bff is ready";
 done;
