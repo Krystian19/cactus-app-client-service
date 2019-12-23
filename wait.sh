0b85f10eedfcbc13b5fa6fe777d11149ab803c03
Jan Guzman <janfrancisco19@gmail.com>
2019-12-23 10:49:51 +0100

    Improve the waiting script for the travis pipeline

--- wait.sh
+++ wait.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ "$1" == "" ]; then
+  echo "No URL provided to wait for"
+  echo exit
+fi
+
 # Waits until cactus.bff is ready to accept connections (Only used inside of travis.ci).
-while ! echo exit | curl -I --fail "http://localhost:4000";
+while ! echo exit | curl -I --fail $1;
 do
-  echo "Waiting for cactus.bff to start"
+  echo "Waiting for $1 to be available"
   ((c++)) && ((c==10)) && break
   sleep 10
 done
